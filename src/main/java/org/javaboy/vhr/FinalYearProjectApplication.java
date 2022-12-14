7fc0ac92d89e105b46a8340de7e56f97e9e82fe3
liuwenshuai <760013065@qq.com>
2022-12-14 09:47:33 +0800

    Initial commit

---
+++ src/main/java/org/javaboy/vhr/FinalYearProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.finalyearproject;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalYearProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalYearProjectApplication.class, args);
+    }
+
+}
